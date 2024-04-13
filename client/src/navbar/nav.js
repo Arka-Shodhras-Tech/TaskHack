a770c499c4dfd77774d2e017a445874ea24b107b
Teja <tejasimma033@gmail.com>
2024-04-13 11:38:29 +0530

    nav.js

--- client/src/navbar/nav.js
+++ client/src/navbar/nav.js
@@ -6,7 +6,7 @@ export const Nav=()=>
         <>
         <div className="deptname" style={{display:'flex',justifyContent:"center"}}>
             <img src={image} className="srkrlogo" alt="logo"/>
-            <h1 className="clgdept">AST CLUB EXAMS</h1>
+            <h1 className="clgdept">ASTEAM EXAMS</h1>
         </div>
         </>
     )
