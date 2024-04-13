48a20762166173fbe5cf0478d85c791088188ea7
Teja <tejasimma033@gmail.com>
2024-04-13 11:37:04 +0530

    ast club exams

--- client/src/navbar/nav.js
+++ client/src/navbar/nav.js
@@ -1,12 +1,12 @@
 import React from "react";
-import image from "../app/srkrlogo.png"
+import image from "../app/srkrlogo.png";
 export const Nav=()=>
 {
     return(
         <>
         <div className="deptname" style={{display:'flex',justifyContent:"center"}}>
-            <img src={image} className="srkrlogo"/>
-            <h1 className="clgdept">DEPARTMENT OF CHEMISTRY</h1>
+            <img src={image} className="srkrlogo" alt="logo"/>
+            <h1 className="clgdept">AST CLUB EXAMS</h1>
         </div>
         </>
     )
