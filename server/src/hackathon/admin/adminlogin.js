5b586fb12ad8db3063a440c9a7db40c162d6179e
sailakshmi004 <130997937+sailakshmi004@users.noreply.github.com>
2024-06-02 18:09:20 +0530

    Merge branch 'main' of https://github.com/Teja080910/Chemistry-dept into fix-sai

---
+++ server/src/hackathon/admin/adminlogin.js
@@ -0,0 +1,19 @@
+import { db } from "../../db.js";
+export const AdminLogin = async (data, res) => {
+    const { email, password } = data;
+    try {
+        const admin = await db.collection('Hacthonadmin').findOne({ Gmail: email });
+        if (!admin) {
+            return res.json({ error: 'Invalid admin' });
+        }
+        if (admin?.Password === password) {
+            return res.json({ message: "admin login sucessfully", data: admin })
+        }
+        else {
+            return res.json({ error: "incorrect password", data: admin })
+        }
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: 'Error during signin' });
+    }
+}
--- server/src/hackathon/message/message.js
+++ server/src/hackathon/message/message.js
@@ -1,5 +1,5 @@
 export const message = {
-    html: async (name,password,regnumber) => `<div style="border: 5px blue solid;width: 100%;">
+    html: async (name, password, regnumber) => `<div style="border: 5px blue solid;width: 100%;">
         <p>
         <h4 style="text-align: center;">This <b>Message</b> From Team AST</h4>
         </p>
@@ -20,4 +20,46 @@ export const message = {
             <h3>Please login here :: <a href='https://asteam-attendance.vercel.app/scrummaster'
                     style="color:black;">Login Here</a></h3>
         </div>
-    </div>`}
+    </div>`,
+
+    otp: async (name, otp, email) => `<div style="border: 5px blue solid;width: 100%;">
+        <p>
+        <h4 style="text-align: center;">This <b>Message</b> From Team AST</h4>
+        </p>
+        <p>
+        <h1 style="text-align: center;">VEDIC VISION HACKATHON</h1>
+        </p>
+        <div style="text-align: center;">
+            Welcome to vedic vision hackathon ${name}
+            <br />
+            <h3>Your password update details</h3>
+        </div>
+        <div style="text-align: center;">
+            <h1>Your Gmail :: ${email}</h1>
+            <h1>Your OTP :: ${otp}</h1>
+        </div>
+        <br />
+    </div>`,
+
+    senlink: async (name,email) => `<div style="border: 5px blue solid;width: 100%;">
+        <p>
+        <h4 style="text-align: center;">This <b>Message</b> From Team AST</h4>
+        </p>
+        <p>
+        <h1 style="text-align: center;">VEDIC VISION HACKATHON</h1>
+        </p>
+        <div style="text-align: center;">
+            Welcome to vedic vision hackathon ${name}
+            <br />
+            <h3>Your login details</h3>
+        </div>
+        <div style="text-align: center;">
+            <h1>Your id :: ${email}</h1>
+        </div>
+        <br />
+        <div style="text-align: center;">
+            <h3>Password update link :: <a href='https://asteam-attendance.vercel.app/scrummaster'
+                    style="color:black;">click Here</a></h3>
+        </div>
+    </div>`
+}
