b0e169a4b72c26c5a80aa8da8436d962a3434940
sailakshmi004 <130997937+sailakshmi004@users.noreply.github.com>
2024-06-19 21:59:53 +0530

    requestform

--- server/src/hackathon/server.js
+++ server/src/hackathon/server.js
@@ -193,4 +193,17 @@ app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
 
+app.post('/createteam/:team/:gmail/:code/:phone', async (req, res) => {
+    const user = await db1.collection('Hackathondata').findOne({ Team: req.params.team });
+    if (user?.Team) {
+        res.json({ team: "already exist", data: user });
+    }
+    else {
+        await db1.collection('Hackathondata').insertOne({ Team: req.params.team, Gmail: req.params.gmail, Phone: req.params.phone ,Code: req.params.code})
+            .then((details) => {
+                res.json({ message: "sucess", data: details });
+            })
+            .catch((e) => console.log(e))
+    }
+})
 export default app
