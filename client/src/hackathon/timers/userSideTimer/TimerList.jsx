c792d3ef179d0a08984bcefed5b13d91bea5a9ed
anilreddykota <22b91a5469@srkrec.ac.in>
2024-06-20 00:46:21 +0530

    added timer module for users to use

---
+++ client/src/hackathon/timers/userSideTimer/TimerList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import TimerItem from "./TimerItem"; // Import your TimerItem component
+import "./Timer.css";
+import timer from "../icons/timer.svg";
+import close from "../icons/close.svg";
+
+const TimerListSidebar = ({ timers, isOpen, toggleSidebar }) => {
+  return (
+    <div className={`timer-list-sidebar ${isOpen ? "open" : ""}`}>
+      <button className="close-btn" onClick={toggleSidebar}>
+        {isOpen ?<img src={close} alt="Open sidebar" /> : <img src={timer} alt="Open sidebar" />}
+      </button>
+      <div className="sidebar-content">
+        <h2 className="h1-animation">Timers </h2>
+        <hr></hr>
+       
+        <div className="timer-list">
+          {timers.map((timer) => (
+            <TimerItem
+              key={timer.id}
+              {...timer}
+              toggleSidebar={toggleSidebar}
+            />
+          ))}
+        </div>
+      </div>
+     
+    </div>
+  );
+};
+
+export default TimerListSidebar;
