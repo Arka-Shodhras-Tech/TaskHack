2965d26f48a4f33ed6b9728466d5d4ded525723a
Teja <tejasimma033@gmail.com>
2024-07-26 00:11:57 +0530

    update database

--- server/src/db.js
+++ server/src/db.js
@@ -9,7 +9,7 @@ async function connectToDB(cb) {
         const client = new MongoClient(url);
         await client.connect();
         db = client.db("ChemistryDepartment");
-        db1 = client.db("Hackathon");
+        db1 = client.db("VedicVision");
         bucket = new GridFSBucket(db1, { bucketName: 'uploads' });
         cb();
     } catch (error) {
