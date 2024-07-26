f9e1f160fa7293590569cdb2f489faadff4375db
anilreddykota <22b91a5469@srkrec.ac.in>
2024-07-26 23:08:43 +0530

    changed timer server to vercel prod

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = "https://timer-server-edko.onrender.com"
+const URL = "https://timer-server-eta.vercel.app"
 
 export const socket = io(URL);
 export {URL};
